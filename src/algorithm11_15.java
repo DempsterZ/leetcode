6b03441d83cbd18ca6fc60870da92e96214daac0
zhuang_cong <zhuangc@disanbo.com>
2018-12-07 09:51:55 +0800

    2018/12/7 9:51

---
+++ src/algorithm11_15.java
@@ -0,0 +1,14 @@
+public class algorithm11_15 {
+
+    public static void main(String[] args) {
+        // 11.盛最多水的容器
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(); maxArea(height);
+    }
+
+    // 11.盛最多水的容器
+    public static int maxArea(int[] height) {
+
+        return 0;
+    }
+}
